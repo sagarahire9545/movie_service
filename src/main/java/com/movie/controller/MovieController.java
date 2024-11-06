d29c24bf0707acd1f059e28fe92ac3b26332d6cd
sagara <sahire@1gen.io>
2024-11-06 16:31:52 +0530

    add mapping annotation

--- src/main/java/com/movie/controller/MovieController.java
+++ src/main/java/com/movie/controller/MovieController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -25,6 +26,7 @@ public class MovieController {
     @Autowired //inject service into controller
     MovieService movieService;
 
+    @PostMapping("/addMovie")
     public ResponseEntity<BaseResponse> addMovie(HttpServletRequest request, HttpServletResponse response,
                                                  @RequestParam(required = false) String title,
                                                  @RequestParam(required = false) String description,
